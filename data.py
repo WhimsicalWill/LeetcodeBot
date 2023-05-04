bfab575acd91653906ecf942e9e966cd39d15919
whimsicalwill <knipewilliam20@gmail.com>
2023-05-03 20:41:38 -0400

    Track data and create progress viz

--- data.py
+++ data.py
@@ -2,52 +2,7 @@ import matplotlib.pyplot as plt
 import sqlite3
 import datetime
 import numpy as np
-
-def get_user_progress(db_conn, user_id):
-    with db_conn:
-        return db_conn.execute("""
-        SELECT date(timestamp, 'start of day') as date, problem_id, percentile
-        FROM user_problems
-        WHERE user_id = ?
-        GROUP BY date, problem_id
-        ORDER BY date
-        """, (user_id,)).fetchall()
-
-def plot_user_progress(db_conn, user_id):
-    user_progress = get_user_progress(db_conn, user_id)
-    dates = []
-    easy_count, medium_count, hard_count = [], [], []
-
-    easy, medium, hard = 0, 0, 0
-
-    for i, entry in enumerate(user_progress):
-        date, problem_id, percentile = entry
-        date = datetime.datetime.strptime(date, '%Y-%m-%d')
-
-        problem = api_instance.api_problems_problem_id_get(problem_id) # Get problem details
-        if problem.difficulty.level == 1: # Easy
-            easy += 1
-        elif problem.difficulty.level == 2: # Medium
-            medium += 1
-        elif problem.difficulty.level == 3: # Hard
-            hard += 1
-
-        dates.append(date)
-        easy_count.append(easy)
-        medium_count.append(medium)
-        hard_count.append(hard)
-
-    plt.plot(dates, easy_count, label='Easy')
-    plt.plot(dates, medium_count, label='Medium')
-    plt.plot(dates, hard_count, label='Hard')
-
-    plt.xlabel('Date')
-    plt.ylabel('Problems Solved')
-    plt.title('User Progress')
-    plt.legend()
-
-    plt.savefig('progress.png')
-    plt.close()
+from collections import defaultdict
 
 def init_db(db_name="user_problems.db"):
     conn = sqlite3.connect(db_name)
@@ -66,7 +21,17 @@ def init_db(db_name="user_problems.db"):
         user_id INTEGER NOT NULL,
         problem_id INTEGER NOT NULL,
         percentile REAL NOT NULL,
+        timestamp DATE NOT NULL,
+        difficulty INTEGER NOT NULL,
         FOREIGN KEY (user_id) REFERENCES users (user_id)
+    );
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS leetcode_problems (
+        problem_id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        difficulty TEXT NOT NULL
     );
     """)
 
@@ -78,13 +43,21 @@ def add_user(db_conn, user_id, username):
     with db_conn:
         db_conn.execute("INSERT OR IGNORE INTO users (user_id, username) VALUES (?, ?)", (user_id, username))
 
-def add_user_problem(db_conn, user_id, problem_id, percentile):
+def add_user_problem(db_conn, user_id, problem_id, percentile, timestamp, difficulty):
+    print(f"Adding user problem: {user_id}, {problem_id}, {percentile}, {timestamp}, {difficulty}")
     with db_conn:
-        db_conn.execute("INSERT INTO user_problems (user_id, problem_id, percentile) VALUES (?, ?, ?)", (user_id, problem_id, percentile))
+        db_conn.execute("INSERT INTO user_problems (user_id, problem_id, percentile, timestamp, difficulty) VALUES (?, ?, ?, ?, ?)", (user_id, problem_id, percentile, timestamp, difficulty))
 
-def get_user_problems(db_conn, user_id):
+def get_problem_difficulty_by_id(db_conn, problem_id):
     with db_conn:
-        return db_conn.execute("SELECT problem_id, percentile FROM user_problems WHERE user_id = ?", (user_id,)).fetchall()
+        result = db_conn.execute("SELECT difficulty FROM leetcode_problems WHERE problem_id = ?", (problem_id,)).fetchone()
+    if result is None:
+        raise Exception(f"Problem {problem_id} not found in database")
+    return result[0]
+
+def add_leetcode_problem(db_conn, problem_id, title, difficulty):
+    with db_conn:
+        db_conn.execute("INSERT OR IGNORE INTO leetcode_problems (problem_id, title, difficulty) VALUES (?, ?, ?)", (problem_id, title, difficulty))
 
 def get_user_problem_counts(db_conn):
     with db_conn:
@@ -96,8 +69,19 @@ def get_user_problem_counts(db_conn):
         ORDER BY problem_count DESC
         """).fetchall()
 
-def plot_leaderboard():
-    user_problem_counts = get_user_problem_counts()
+def get_user_progress(db_conn, user_id):
+    with db_conn:
+        return db_conn.execute("""
+        SELECT date(timestamp, 'start of day') as date, difficulty, COUNT(DISTINCT problem_id) as problem_count
+        FROM user_problems
+        WHERE user_id = ?
+        GROUP BY date, difficulty
+        ORDER BY date
+        """, (user_id,)).fetchall()
+
+def plot_leaderboard(db_conn):
+    user_problem_counts = get_user_problem_counts(db_conn)
+    print(user_problem_counts)
     usernames = [user[0] for user in user_problem_counts]
     problem_counts = [user[1] for user in user_problem_counts]
 
@@ -106,4 +90,38 @@ def plot_leaderboard():
     plt.ylabel('Problems Solved')
     plt.title('Number of Problems Solved by Each User')
     plt.savefig('bar_chart.png')
+    plt.close()
+
+def plot_user_progress(db_conn, user_id):
+    user_progress = get_user_progress(db_conn, user_id)
+    problems_per_day = defaultdict(lambda: [0, 0, 0]) # date -> [easy, medium, hard]
+
+    for date_str, difficulty, count in user_progress:
+        date = datetime.datetime.strptime(date_str, '%Y-%m-%d').date()
+
+        if difficulty == 1: # Easy
+            problems_per_day[date][0] = count
+        elif difficulty == 2: # Medium
+            problems_per_day[date][1] = count
+        elif difficulty == 3: # Hard
+            problems_per_day[date][2] = count
+
+    dates = problems_per_day.keys()
+    easy_count = [problems_per_day[date][0] for date in dates]
+    medium_count = [problems_per_day[date][1] for date in dates]
+    hard_count = [problems_per_day[date][2] for date in dates]
+
+    # Set x-axis tick labels and rotation angle
+    plt.xticks(rotation=90)
+
+    plt.plot(dates, easy_count, label='Easy')
+    plt.plot(dates, medium_count, label='Medium')
+    plt.plot(dates, hard_count, label='Hard')
+
+    plt.xlabel('Date')
+    plt.ylabel('Problems Solved')
+    plt.title('User Progress')
+    plt.legend()
+
+    plt.savefig('progress.png')
     plt.close()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
 import json
 import discord
+import datetime
 from discord.ext import commands
 
 import problems
@@ -19,6 +19,7 @@ def run_discord_bot():
 
     # Initialize the database
     db_conn = data.init_db()
+    print("Database initialized.")
 
     @bot.event
     async def on_ready():
@@ -26,24 +27,51 @@ def run_discord_bot():
 
     @bot.command(name='leet')
     async def leet(ctx):
-        # Get the random unsolved question and send it
-        _, problem_url = problems.get_random_unsolved_question(leetcode_api)
-        await ctx.send(problem_url)
+        """Provides the daily leetcode problems."""
+
+        # Get a random unsolved question for each difficulty level
+        difficulty_levels = [key.title() for key, value in config["problem_difficulties"].items() if value]
+        
+        print(f"Fetching random unsolved problems for difficulty levels: {difficulty_levels}")
+        leet_problems = problems.get_random_unsolved_questions(leetcode_api, difficulty_levels)
+        print(f"Found {len(leet_problems)} random unsolved problems.")
+        print(leet_problems)
+
+        # Format the problem information as a message
+        message = f"{ctx.author.mention} Here are today's problems:\n\n"
+        for difficulty, problem_info in leet_problems.items():
+            problem_id = problem_info['id']
+            problem_level = problem_info['level']
+            problem_title = problem_info['title']
+            acceptance_rate = problem_info['acceptance_rate']
+            data.add_leetcode_problem(db_conn, problem_id, problem_title, problem_level)
+            print(f"Added problem {problem_id}, of difficulty {difficulty} to database.")
+            message += f"**{difficulty} [#{problem_id}]:** **{problem_title}**, ({acceptance_rate}%)\n" \
+                       f"(<https://leetcode.com/problems/{problem_title.lower().replace(' ', '-')}/{problem_id}>)\n"
+
+        await ctx.send(message)
 
     @bot.command(name='solved')
     async def report_score(ctx):
+        """Reports the user's performance on a problem."""
         message = ctx.message
         try:
-            _, problem_id, percentile_performance = message.content.split()
-            data.add_user_problem(message.author.id, int(problem_id), float(percentile_performance))
-        except ValueError:
-            await message.channel.send(f"{message.author.mention} Invalid message format.\n\
-                                        Please use: !solved <problem_id> <percentile> (ex: !solved 1 99.9)")
+            _, problem_id, percentile = message.content.split()
+            # TODO: refactor this to level (since level is numeric, difficulty is string)
+            difficulty = data.get_problem_difficulty_by_id(db_conn, problem_id)
+            timestamp = datetime.datetime.now().date()
+            data.add_user_problem(db_conn, message.author.id, int(problem_id), float(percentile), timestamp, difficulty)
+            await message.channel.send(f"{message.author.mention} Successfully added problem {problem_id} with {percentile}% performance.")
+        except Exception as e:
+            print(f"Exception: {e}")
+            await message.channel.send(f"{message.author.mention} Invalid message. Make sure you're reporting a valid problem id.\n"
+                                        "Also, please use: !solved <problem_id> <percentile> (ex: !solved 1 99.9)")
 
     @bot.command(name='leaderboard')
     async def leaderboard(ctx):
+        """Provides a leaderboard of all active participants."""
         # Create the bar chart and save it to disk
-        data.plot_leaderboard()
+        data.plot_leaderboard(db_conn)
 
         # Send the image file as a message in the Discord channel
         with open('bar_chart.png', 'rb') as img_file:
@@ -51,19 +79,12 @@ def run_discord_bot():
 
     @bot.command(name='progress')
     async def progress(ctx):
+        """Plots the user's progress over time."""
         user_id = ctx.author.id
         data.plot_user_progress(db_conn, user_id)
 
         with open('progress.png', 'rb') as img_file:
             await ctx.send(file=discord.File(img_file, 'progress.png'))
-
-    @bot.command(name='help')
-    async def help(ctx):
-        await ctx.send(f"{ctx.author.mention} Commands:\n\
-                        !leet - get a random unsolved LeetCode problem\n\
-                        !solved <problem_id> <percentile> - report your performance on a problem\n\
-                        !leaderboard - see the leaderboard\n\
-                        !progress - see your progress")
 
     bot.run(config['TOKEN'])
 
